122c9875399b2c1d83ff7a360ae7211170b3eb61
bora <youonl5@naver.com>
2023-12-22 15:23:33 +0900

    Feat: TODO List 추가

--- src/main/java/com/hackathon/heardf/domain/braille/BrailleController.java
+++ src/main/java/com/hackathon/heardf/domain/braille/BrailleController.java
@@ -8,4 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BrailleController {
 
     private final BrailleService brailleService;
+
+    /**
+     * TODO: [POST] TEXT -> 점자 변환하기 (GPT 이용)
+     */
+
+    /**
+     * TODO: [GET] 점자 조회하기
+     */
 }
--- src/main/java/com/hackathon/heardf/domain/pdf/PdfController.java
+++ src/main/java/com/hackathon/heardf/domain/pdf/PdfController.java
@@ -8,4 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PdfController {
 
     private final PdfService pdfService;
+
+    /**
+     * TODO: [POST] PDF -> TEXT 변환하기 (ASPOSE 이용)
+     */
+
+    /**
+     * TODO: [GET] TEXT 조회하기
+     */
 }
