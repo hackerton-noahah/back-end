b78c383db91813225a65edd471e35161fb699b75
bora <youonl5@naver.com>
2023-12-22 11:56:59 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/hackathon/heardf/HeardfApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.heardf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeardfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeardfApplication.class, args);
+	}
+
+}
